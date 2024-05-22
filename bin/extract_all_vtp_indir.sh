6a99b512b08cb4cd12f3aac1d93557234e503921
Thomas Madison <56737848+madisoth@users.noreply.github.com>
2024-05-22 16:29:31 -0700

    Update extract_all_vtp_indir.sh
    add FS -> gifti conversion and patch GeometricType into GIFTI XML to work with wb_view

--- bin/extract_all_vtp_indir.sh
+++ bin/extract_all_vtp_indir.sh
@@ -8,7 +8,7 @@ fi
 
 MESHDIR=$2
 VOLGEOM=$1
-PARALLEL=YES
+PARALLEL=""
 T=.commands
 if [ "$PARALLEL" == "YES" ]
 then
@@ -20,17 +20,24 @@ for i in $MESHDIR/*.vtp
 do
 	B=`basename $i`
 	
-	CMD="./VTPExtractAll --surf-volgeom=$VOLGEOM $MESHDIR/$B"
-	if [ "$PARALLEL" == "YES" ]
-	then
-		echo $CMD >> $T
-	else
-		$CMD
-	fi
+#	CMD="/opt/MCRIBS/bin/VTPExtractAll -v --surf-volgeom=$VOLGEOM $MESHDIR/$B"
+#	if [ "$PARALLEL" == "YES" ]
+#	then
+#		echo $CMD >> $T
+#	else
+#		$CMD
+#	fi
+        /opt/MCRIBS/VTK/VTK-install/bin/vtkpython /opt/MCRIBS/bin/VTPExtractAll -v --surf-volgeom=$VOLGEOM $MESHDIR/$B
+        mris_convert --to-scanner $MESHDIR/${B::-4}_tkr.surf $MESHDIR/${B::-4}_tkr.surf.gii
+
+        sed '0,/<Name><!\[CDATA\[VolGeomWidth\]\]><\/Name>/s|<Name><!\[CDATA\[VolGeomWidth\]\]></Name>|<Name><![CDATA[GeometricType]]></Name>\n<Value><![CDATA[Anatomical]]></Value>\n</MD>\n<MD>\n<Name><![CDATA[VolGeomWidth]]></Name>|' $MESHDIR/${B::-4}_tkr.surf.gii > $MESHDIR/${B::-4}_tkr_fixxml.surf.gii
+
+        
+        
 done
 
-if [ "$PARALLEL" == "YES" ]
-then
-	parallel -j+0 --ungroup < $T
-	rm -f $T
-fi
+#if [ "$PARALLEL" == "YES" ]
+#then
+#	parallel -j+0 --ungroup < $T
+#	rm -f $T
+#fi
